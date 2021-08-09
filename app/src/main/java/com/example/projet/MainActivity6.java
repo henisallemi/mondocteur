b922fe00829fedbae6c9ab53871c85bb9ae67ab0
unknown <heni.slim.sallemi@gmail.com>
2021-08-09 18:27:49 +0200

    menu

---
+++ app/src/main/java/com/example/projet/MainActivity6.java
@@ -0,0 +1,32 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+
+        final DrawerLayout drawerLayout;
+        drawerLayout = findViewById(R.id.drawerlayout);
+        findViewById(R.id.imageview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView= findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+    }
+}
